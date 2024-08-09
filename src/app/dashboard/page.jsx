fe1ff195ed41ec4de758c4d91f7a1279be8f9128
Wechuli Simiyu <wechuli017@gmail.com>
2024-08-09 12:34:45 +0300

    add initial UI

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./page.module.css";
 
 const Dashboard = () => {
   return (
-    <div>
-      DASHBOARD
+    <div className={styles.container}>
+      Dashboard
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          A simple design with Next js.
+        </h1>
+        <p className={styles.desc}>
+          Trying out blog functionality. With a portfolio. Will be able to fetch data soon.
+        </p>
+        <Button url="/portfolio" text="See Our Works"/>
+      </div>
+      <div className={styles.item}>
+        <Image src="/hero.png" 
+          alt="Hero Image" 
+          className={styles.img} 
+          width={1000} 
+          height={500} />
+      </div>
     </div>
   );
 }
