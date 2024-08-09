e4b5f104bfcd27b35d823ef11713278088b61d35
Wechuli Simiyu <wechuli017@gmail.com>
2024-08-09 12:35:21 +0300

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from "./footer.module.css";
+import Image from 'next/image';
+
 
 const Footer = () => {
   return (
-    <div>
-      ©2023 Previous. All rights reserved.
+    <div className={styles.container}>
+      <div>©2024 Previous. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Previous"/>
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+      </div>
     </div>
   )
 }
